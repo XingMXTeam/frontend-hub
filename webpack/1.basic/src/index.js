b19e259dcfd04a949183d23a2868f1903d4dc326
xunxing.mao <xunxing.mao@crgecent.com>
2019-06-15 23:59:10 +0800

    update

--- webpack/1.basic/src/index.js
+++ webpack/1.basic/src/index.js
@@ -1,12 +1,24 @@
-import _ from 'lodash';
-import './style.css'
+// import printMe from './print.js'
+
+// function component() {
+//     let element = document.createElement('div');
+//     var btn = document.createElement('button');
+//     btn.innerHTML = 'Click me and check the console!';
+//     btn.onclick = printMe;
+
+//     element.appendChild(btn);
+//     return element;
+// }
+import css from './index.css'
+
+import Icon from '../asset/a.jpg';
 
 function component() {
     let element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
+    let myIcon = new Image()
+    myIcon.src = Icon
+    element.appendChild(myIcon)
 
     return element;
 }
--- webpack/1.basic/webpack.config.js
+++ webpack/1.basic/webpack.config.js
@@ -1,9 +1,52 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const postcssPxToViewport = require('postcss-px-to-viewport');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'output management'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[hash].[ext]',
+                },
+            }]
+        }, {
+            test: /\.css$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'postcss-loader',
+                options: {
+                    plugins: (loader) => [
+                        require('postcss-px-to-viewport')({
+                            viewportWidth: 750, //根据视觉稿的宽度进行设置
+                            viewportHeight: 1334,
+                            unitPrecision: 5,
+                            viewportUnit: 'vw',
+                            selectorBlackList: [], //忽略转换的css选择器
+                            minPixelValue: 1,
+                            mediaQuery: false
+                        }),
+                    ]
+                }
+            }],
+        }]
     }
 };
